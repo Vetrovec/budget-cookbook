0c2508bf0dd327b26136f2f4e8512d6f074f8d7b
Martin Havelka <martin.havelka@outreach.io>
2023-05-02 21:35:13 +0200

    feat: use prettier to format source code

--- server/dao/RecipeIngredientDao.js
+++ server/dao/RecipeIngredientDao.js
@@ -1,14 +1,14 @@
-const dbConnection = require('../dbConnection');
+const dbConnection = require("../dbConnection");
 
 class RecipeDao {
-	constructor(db) {
-		this.db = db;
-	}
+  constructor(db) {
+    this.db = db;
+  }
 
-	createTable() {
-		return new Promise((resolve, reject) => {
-			this.db.run(
-				`CREATE TABLE IF NOT EXISTS recipe_ingredient (
+  createTable() {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        `CREATE TABLE IF NOT EXISTS recipe_ingredient (
 					recipe_id INTEGER,
 					ingredient_id INTEGER,
 					amount REAL,
@@ -16,56 +16,56 @@ class RecipeDao {
 					FOREIGN KEY (recipe_id) REFERENCES recipeDao(id) ON DELETE CASCADE,
 					FOREIGN KEY (ingredient_id) REFERENCES ingredientDao(id) ON DELETE RESTRICT
 				)`,
-				(err) => {
-					if (err) {
-						reject(err);
-						return;
-					}
-					resolve();
-				},
-			);
-		});
-	}
+        (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve();
+        }
+      );
+    });
+  }
 
-	getRecipeIngredients(recipeId) {
-		return new Promise((resolve, reject) => {
-			this.db.all(
-				`SELECT *
+  getRecipeIngredients(recipeId) {
+    return new Promise((resolve, reject) => {
+      this.db.all(
+        `SELECT *
 				FROM recipe_ingredient
 				LEFT JOIN ingredient ON recipe_ingredient.ingredient_id = ingredient.id
 				WHERE recipe_ingredient.recipe_id = ?`,
-				[recipeId],
-				(err, rows) => {
-					if (err) {
-						reject(err);
-						return;
-					}
-					resolve(rows);
-				},
-			);
-		});
-	}
+        [recipeId],
+        (err, rows) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(rows);
+        }
+      );
+    });
+  }
 
-	// Create a new recipe
-	create(recipeIngredient) {
-		return new Promise((resolve, reject) => {
-			this.db.run(
-				'INSERT INTO recipe_ingredient (recipe_id, ingredient_id, amount) VALUES (?, ?, ?)',
-				[
-					recipeIngredient.recipeId,
-					recipeIngredient.ingredientId,
-					recipeIngredient.amount,
-				],
-				function (err) {
-					if (err) {
-						reject(err);
-						return;
-					}
-					resolve(this.lastID);
-				},
-			);
-		});
-	}
+  // Create a new recipe
+  create(recipeIngredient) {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        "INSERT INTO recipe_ingredient (recipe_id, ingredient_id, amount) VALUES (?, ?, ?)",
+        [
+          recipeIngredient.recipeId,
+          recipeIngredient.ingredientId,
+          recipeIngredient.amount,
+        ],
+        function (err) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(this.lastID);
+        }
+      );
+    });
+  }
 }
 
 module.exports = new RecipeDao(dbConnection);
--- server/index.js
+++ server/index.js
@@ -1,51 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const recipeRouter = require('./controllers/recipe');
-const ingredientRouter = require('./controllers/ingredient');
-const ingredientDao = require('./dao/IngredientDao');
-const recipeDao = require('./dao/RecipeDao');
-const recipeIngredientDao = require('./dao/RecipeIngredientDao');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const recipeRouter = require("./controllers/recipe");
+const ingredientRouter = require("./controllers/ingredient");
+const ingredientDao = require("./dao/IngredientDao");
+const recipeDao = require("./dao/RecipeDao");
+const recipeIngredientDao = require("./dao/RecipeIngredientDao");
 
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 
 async function main() {
-	await ingredientDao.createTable();
-	await recipeDao.createTable();
-	await recipeIngredientDao.createTable();
+  await ingredientDao.createTable();
+  await recipeDao.createTable();
+  await recipeIngredientDao.createTable();
 
-	let uploadDir = path.join(__dirname, '/upload')
-	if (!fs.existsSync(uploadDir)) {
-		fs.mkdirSync(uploadDir, 0o744);
-	}
+  let uploadDir = path.join(__dirname, "/upload");
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, 0o744);
+  }
 
-	console.log("Created");
+  console.log("Created");
 
-	const app = express();
-	// Use the express-fileupload middleware
-	app.use(fileUpload({
-		limits: {
-			fileSize: 10000000,
-		},
-		abortOnLimit: true,
-	}));
+  const app = express();
+  // Use the express-fileupload middleware
+  app.use(
+    fileUpload({
+      limits: {
+        fileSize: 10000000,
+      },
+      abortOnLimit: true,
+    })
+  );
 
+  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "public")));
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
 
-	app.use(express.static(path.join(__dirname, 'build')));
-	app.use(express.static(path.join(__dirname, 'public')));
-	app.get('/', function (req, res) {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'));
-	});
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+  app.use("/ingredient", ingredientRouter);
+  app.use("/recipe", recipeRouter);
 
-	app.use('/ingredient', ingredientRouter);
-	app.use('/recipe', recipeRouter);
-
-	app.listen(3000, () => {
-		console.log('Express server listening on port 3000.');
-	});
+  app.listen(3000, () => {
+    console.log("Express server listening on port 3000.");
+  });
 }
 
 main();
